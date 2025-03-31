b83aee9b6b68a961f7b3fa504ab7e3541b1c35ce
bartosz <bartosz_magister@protonmail.com>
2025-03-31 23:54:08 +0200

    Bartek PRE ALPAHA 0.0.1.1

---
+++ TickerK8/TickerK8.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Aktywowanie wirtualnego środowiska
+source /home/admin/PycharmProjects/TickerK8/.venv/bin/activate
+
+# Uruchomienie aplikacji
+python /home/admin/PycharmProjects/TickerK8/TickerK8_app/app_files/PYTHON/_0000_app_base.py
